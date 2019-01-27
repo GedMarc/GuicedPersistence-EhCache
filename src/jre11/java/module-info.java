f257b3808ee92cd98faaaf55bc7d62e160186075
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:26 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,11 +1,3 @@
-import com.jwebmp.guicedinjection.interfaces.IGuiceDefaultBinder;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.guicedpersistence.ehcache.EhCacheBinder;
-import com.jwebmp.guicedpersistence.ehcache.EhCacheEntityManagerProperties;
-import com.jwebmp.guicedpersistence.ehcache.EhCacheModuleExclusions;
-import com.jwebmp.guicedpersistence.services.IPropertiesEntityManagerReader;
-
 module com.jwebmp.guicedpersistence.ehcache {
 	exports com.jwebmp.guicedpersistence.ehcache;
 
@@ -18,10 +10,10 @@ module com.jwebmp.guicedpersistence.ehcache {
 	requires com.jwebmp.guicedpersistence.readers.hibernateproperties;
 	requires java.validation;
 
-	provides IGuiceDefaultBinder with EhCacheBinder;
-	provides IPropertiesEntityManagerReader with EhCacheEntityManagerProperties;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceDefaultBinder with com.jwebmp.guicedpersistence.ehcache.EhCacheBinder;
+	provides com.jwebmp.guicedpersistence.services.IPropertiesEntityManagerReader with com.jwebmp.guicedpersistence.ehcache.EhCacheEntityManagerProperties;
 
-	provides IGuiceScanModuleExclusions with EhCacheModuleExclusions;
-	provides IGuiceScanJarExclusions with EhCacheModuleExclusions;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.guicedpersistence.ehcache.EhCacheModuleExclusions;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.guicedpersistence.ehcache.EhCacheModuleExclusions;
 
 }
